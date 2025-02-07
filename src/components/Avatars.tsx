8e676660c70e06ac05a64fb5a26475fd842d18ca
Darshn-26 <darshanpr26@gmail.com>
2025-02-07 17:08:35 +0530

    Some update

--- src/components/Avatars.tsx
+++ src/components/Avatars.tsx
@@ -1,46 +1,51 @@
 "use client";
-import { useOthers, useSelf } from '@liveblocks/react/suspense';
-import React from 'react'
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-  } from "@/components/ui/tooltip"
-  import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-  
+import { useOthers, useSelf } from '@liveblocks/react/suspense';
+import React from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function Avatars() {
-    const others = useOthers();
-    const self = useSelf();
+  const others = useOthers();
+  const self = useSelf();
 
-    const all=[self,...others];
-
+  // Filter out any invalid users and combine self with others
+  const allUsers = [self, ...others].filter(user => 
+    user && user.info && user.id && user.info.name
+  );
 
   return (
     <div className='flex items-center gap-2'>
-        <p className='text-sm font-light'>User's currently online</p>
-        <div className='flex -space-x-5'>
-            {all.map((others,i)=>
-            <TooltipProvider key={others?.id + i }>
+      <p className='text-sm font-light'>Users currently online</p>
+      <div className='flex -space-x-5'>
+        {allUsers.map((user, index) => (
+          <TooltipProvider key={`user-${user.id}-${index}`}>
             <Tooltip>
               <TooltipTrigger>
                 <Avatar className="border-2 hover:z-50">
-                  <AvatarImage src={others?.info?.avatar} /> 
-                  <AvatarFallback>{others?.info?.name}</AvatarFallback>
+                  <AvatarImage 
+                    src={user.info?.avatar} 
+                    alt={user.info?.name}
+                  />
+                  <AvatarFallback>
+                    {user.info?.name?.[0]?.toUpperCase() || '?'}
+                  </AvatarFallback>
                 </Avatar>
               </TooltipTrigger>
               <TooltipContent>
-                <p>{self?.id === others?.id ? "You" : others?.info.name}</p>
+                <p>{self.id === user.id ? "You" : user.info.name}</p>
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
-          )}
-
-        </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Avatars
+export default Avatars;
